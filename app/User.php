3dbb61ee9e4013cfbbb9254372178f51cc96f44e
Muhammad Faryad <muhammad.faryad2006@gmail.com>
2020-08-17 12:15:44 +0500

    added log functionality using package

--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,6 +12,14 @@ class User extends Authenticatable
 {
     use SoftDeletes;
     use Notifiable;
+    
+    //activity logging starts
+    use LogsActivity;
+    protected static $logAttributes = ['name', 'email'];
+    protected static $logName = 'User';
+    protected static $logOnlyDirty = true;
+    protected static $submitEmptyLogs = false;
+    //activity logging ends
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +29,15 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password','role'
     ];
+
+    //customize Description of activity logs
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return "{$eventName} the user";
+    }
+
+
+
 
     /**
      * The attributes that should be hidden for arrays.
